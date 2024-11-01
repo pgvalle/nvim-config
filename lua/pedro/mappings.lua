eaa661af6949b1f8604341c04a52734278c1bb63
pgvalle <pgvalle.nogueira@gmail.com>
2024-11-01 11:12:04 -0300

    Quit remap

--- lua/pedro/mappings.lua
+++ lua/pedro/mappings.lua
@@ -1,11 +1,10 @@
 -- Needs to be set before initializing lazy
 vim.g.mapleader = ' '
 
-local map = function(mode, cmd, bind)
-  vim.keymap.set(mode, cmd, bind, {silent = true})
+local map = function(mode, bind, cmd)
+  vim.keymap.set(mode, bind, cmd, { noremap = true, silent = true })
 end
 
-map('n', '<C-q>', vim.cmd.quit)
+map('n', '<leader>q', vim.cmd.quit)
 map('n', '<leader>ff', ':Telescope find_files<Enter>')
 
--- vim.ui.input({ prompt = 'Write commit message: ' }, function(input)
