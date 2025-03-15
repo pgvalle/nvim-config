9720a3b81151edf5c36bbb7a7a29704d44e3331f
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-15 18:07:26 -0300

    Removing extensions

--- lua/pedro/plugins/telescope.lua
+++ lua/pedro/plugins/telescope.lua
@@ -4,12 +4,12 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    --{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   config = function()
     local telescope = require('telescope')
     --local actions = require('telescope.actions')
 
-    telescope.load_extension('fzf')
+--    telescope.load_extension('fzf')
   end
 }
