9281518f8d5f6b71c5d1ae1f24edcad563db8aa8
pgvalle <pgvalle.nogueira@gmail.com>
2025-12-25 09:32:31 -0300

    git repo detection

--- lua/pedro/mappings.lua
+++ lua/pedro/mappings.lua
@@ -11,6 +11,14 @@ end
 map('n', '<leader>q', vim.cmd.quit)
 
 -- telescope
-map('n', '<leader>ff', ':Telescope find_files<Enter>')
+map('n', '<leader>ff', function()
+    vim.fn.system("git status")
+    if vim.v.shell_error == 0 then
+        vim.cmd('Telescope git_files')
+    else
+        vim.cmd('Telescope find_files')
+    end
+end)
+
 map('n', '<leader>fb', ':Telescope buffers<Enter>')
 map('n', '<leader>lg', ':Telescope live_grep<Enter>')
